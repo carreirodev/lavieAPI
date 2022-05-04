aa6fee0214f317bd7174a48e6859e9139a490c0a
Gefferson Oliveira <gefferson_oliveira@outlook.com>
2022-05-04 03:02:43 -0300

    Added validation Psicologos.

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,9 @@ const authController =  require('../controllers/authController');
 const authLoginValidation = require('../validator/auth/login');
 const auth = require('../middleware/auth');
 const psicologoCreateValidation = require('../validator/psicologos/create');
-const atendimentoCreateValidation = require('../validator/atendimentos/create')
+const psicologoUpdateValidation = require('../validator/psicologos/update');
+const psicologoSelectIDValidation = require('../validator/psicologos/selectID');
+const atendimentoCreateValidation = require('../validator/atendimentos/create');
 
 const routes = express.Router();
 
@@ -22,11 +24,11 @@ routes.get("/atendimentos/:id", atendimentoController.buscarAtendimento);
 routes.post("/atendimentos", auth, atendimentoCreateValidation, atendimentoController.criarAtendimento);
 
 routes.get("/psicologos", auth, psicologoController.listarPsicologos);
-routes.get("/psicologos/:id", auth, psicologoController.detalhesDoPsicologo);
+routes.get("/psicologos/:id",psicologoSelectIDValidation, auth, psicologoController.detalhesDoPsicologo);
 routes.post("/psicologos", psicologoCreateValidation, psicologoController.registrarPsicologo);
 routes.post("/login", authLoginValidation, authController.login);
-routes.put("/psicologos/:id", auth, psicologoController.atualizarPsicologo);
-routes.delete("/psicologos/:id", auth, psicologoController.deletarPsicologo);
+routes.put("/psicologos/:id", psicologoUpdateValidation, auth, psicologoController.atualizarPsicologo);
+routes.delete("/psicologos/:id",psicologoSelectIDValidation, auth, psicologoController.deletarPsicologo);
 
 
 module.exports = routes;
---
+++ src/validator/psicologos/update.js
@@ -0,0 +1,14 @@
+const { validate, Joi } = require("express-validation");
+
+module.exports = validate({
+    params: Joi.object({
+        id: Joi.string().required(),        
+    }),
+
+    body: Joi.object({
+        nome: Joi.string().required(),
+        email: Joi.string().email().required(),
+        senha: Joi.string().min(6).max(45).required(),
+        apresentacao: Joi.string().max(255).required(),
+    }),
+});
